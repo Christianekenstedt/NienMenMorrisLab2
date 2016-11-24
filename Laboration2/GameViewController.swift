8b1d45f09174f7d978e2b73e2a86df48b1f35c44
Christian <asd>
2016-11-24 17:50:02 +0100

    Started working on game board and added nodes, board positions and player markers for blue and red.

--- Laboration2/GameViewController.swift
+++ Laboration2/GameViewController.swift
@@ -18,17 +18,16 @@ class GameViewController: UIViewController {
         if let view = self.view as! SKView? {
             // Load the SKScene from 'GameScene.sks'
             
-            let scene = GameScene(size: view.bounds.size)
+            //let scene = GameScene(size: view.bounds.size)
             
-            view.presentScene(scene)
+            //view.presentScene(scene)
             
-            /*if let scene = GameScene(size: SKView.bounds.size) {
+            if let scene = SKScene(fileNamed: "GameScene") {
                 // Set the scale mode to scale to fit the window
                 scene.scaleMode = .aspectFill
-                
                 // Present the scene
                 view.presentScene(scene)
-            }*/
+            }
             
             view.ignoresSiblingOrder = true
             
